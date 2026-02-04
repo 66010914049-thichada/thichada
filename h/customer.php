bbb510ed58c9c16245a3a4ce1454ed9d8f72ab79
66010914049-thichada <66010914049@msu.ac.th>
2026-02-04 16:17:47 +0700

    1

---
+++ h/customer.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	if (entry($_SESSION['aid'])) {
+		echo "Access Denied" ;
+		echo "<meta http-equiv='refresh' content='4;url=index.php'>";
+		exit;
+	}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ธิชาดา สีทอน - น้ำตาล</title>
+</head>
+
+<body>
+<h1>จัดการลูกค้า</h1>
+
+<?php echo "แอดมิน: ". $_SESSION['a_name'];?><br>
+
+<ul>
+	<a href="product.php"><li>จัดการสินค้า</li></a>
+    <a href="orders.php"><li>จัดการออเดอร์</li></a>
+	<a href="customer.php"><li>จัดการลูกค้า</li></a>
+    <a href="logout.php"><li>ออกจากระบบ</li></a>
+</ul>
+</body>
+</html>
