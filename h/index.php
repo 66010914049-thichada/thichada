47bc8520e6e0e7f76586bd2f38439bdacc901cfa
66010914049-thichada <66010914049@msu.ac.th>
2026-02-04 14:37:17 +0700

    1

---
+++ h/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ธิชาดา สีทอน(น้ำตาล)</title>
+</head>
+
+<body>
+<h1>หน้าเข้าสู่ระบบ - ธิชาดา</h1>
+
+<form method="post" action="">
+Username<input type="text" name="auser" autofocus required> <br>
+Password<input type="password" name="apwd" required> <br>
+<button type="submit" name="Submit">LOGIN</button>
+</form>
+</body>
+</html>
