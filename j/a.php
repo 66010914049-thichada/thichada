5111c514a5f166de463f29ea19bf183e98b27ad7
thichada <66010914049@msu.ac.th>
2026-02-18 14:06:41 +0700

    1

---
+++ j/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914049 ธิชาดา สีทอน(น้ำตาล)</title>
+</head>
+
+<body>
+<h1>งาน j - ธิชาดา สีทอน(น้ำตาล)</h1>
+<img src="1.jpg" height="400">
+</body>
+</html>
